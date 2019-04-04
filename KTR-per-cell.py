7d0909db64b55299daac35ae2fbb4af4441dc923
leoguignard <leo.guignard@gmail.com>
2019-04-04 06:00:01 -0400

    comments on the KTR-per-cell file

--- KTR-per-cell.py
+++ KTR-per-cell.py
@@ -11,18 +11,25 @@ def main():
     
     args = parser.parse_args()
 
+    #read images
     intensity = imread(args.intensity)
     seg = imread(args.segmentation)
 
+    # compute bounding boxes to speed up the process
     bboxes = nd.find_objects(seg)
+
+    # list the cell ids 
     c_ids = np.unique(seg)
 
+    # build the avarage for each cell id
     avgs = {}
     for c in c_ids:
+        # we assume that 1 is the background
         if c != 1:
             bb = bboxes[c-1]
             avgs[c] = np.mean(intensity[bb][intensity[bb] == c])
 
+    # write the averages in the output file
     with open(args.output, 'w') as f:
         f.write('c_id, avg\n')
         for c, avg in avgs.iteritems():
